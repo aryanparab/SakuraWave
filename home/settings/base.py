8adb76597fdac685f64e4f29d950126c33208cf7
aryanparab <parab.aryan@gmail.com>
2020-12-23 21:50:45 +0530

    please

--- home/settings/base.py
+++ home/settings/base.py
@@ -4,8 +4,8 @@ import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["sakurawave.herokuapp.com" , '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
