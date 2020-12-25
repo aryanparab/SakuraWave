b094c1981ebdf88fff0904df7cf485161eeea638
aryanparab <parab.aryan@gmail.com>
2020-12-25 20:14:34 +0530

    sidf

--- home/settings/base.py
+++ home/settings/base.py
@@ -68,7 +68,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
