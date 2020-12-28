fee3623f95a33929ca27017653d5c0ed59f4c6ff
Aryan Parab <66548981+aryanparab@users.noreply.github.com>
2020-12-28 18:49:18 +0530

    fek

--- home/settings/base.py
+++ home/settings/base.py
@@ -1,7 +1,7 @@
 import os
 import config
-import django-heroku
 
+import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
