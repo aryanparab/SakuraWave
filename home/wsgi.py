c2539132783bc2c3b5959c5df9fe813aba4f8f9b
Aryan Parab <66548981+aryanparab@users.noreply.github.com>
2020-12-28 19:22:43 +0530

    requriemwats.txt

--- home/wsgi.py
+++ home/wsgi.py
@@ -1,4 +1,5 @@
 import os
+
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
@@ -7,7 +8,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
 
 application = get_wsgi_application()
 application = Cling(get_wsgi_application())
-
 application = DjangoWhiteNoise(application)
 
 
