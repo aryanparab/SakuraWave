c9410e37b4d780fe630a24d5d2888fcb6a7f0e8e
Aryan Parab <66548981+aryanparab@users.noreply.github.com>
2020-12-30 01:24:59 +0530

    () wsgi app

--- home/wsgi.py
+++ home/wsgi.py
@@ -1,10 +1,9 @@
 import os
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
 
-application = get_wsgi_application
+application = get_wsgi_application()
 
 
 
